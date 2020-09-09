a132fe067972c625c35e25b6e29566b20bc7ad69
Lucas Allegri <lucasallegri@protonmail.com>
2020-09-09 16:51:52 -0300

    Better formatting on Shhhhhhh.java

--- src/main/java/com/lucasallegri/Shhhhhhh.java
+++ src/main/java/com/lucasallegri/Shhhhhhh.java
@@ -11,54 +11,53 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class Shhhhhhh extends ListenerAdapter {
-    
-    public JDA session;
-    public String token;
-    public String voiceChannelSnowflake;
-    public int[] dead;
-    public int[] crew;
-    
-    public Shhhhhhh(String token) {
-        
-        this.token = token;
-        initSession();
-    }
-    
-    private void initSession() {
-    	
-    	try {
-			this.session = 
-					JDABuilder.createLight(this.token, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
-		            .addEventListeners(this)
-		            .setActivity(Activity.playing("Among Us"))
-		            .build();
+
+	public JDA session;
+	public String token;
+	public String voiceChannelSnowflake;
+	public int[] dead;
+	public int[] crew;
+
+	public Shhhhhhh(String token) {
+
+		this.token = token;
+		initSession();
+	}
+
+	private void initSession() {
+
+		try {
+			this.session = JDABuilder
+					.createLight(this.token, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
+					.addEventListeners(this).setActivity(Activity.playing("Among Us")).build();
 		} catch (LoginException e) {
 			e.printStackTrace();
 		}
-    }
-    
-    public JDA getSession() {
-    	return this.session;
-    }
-    
-    @Override
-    public void onMessageReceived(MessageReceivedEvent event) {
-    	Message msg = event.getMessage();
-    	switch(msg.getContentRaw()) {
-    	case "a.update": 
-    		CommandUpdate.handle(this, event);
-    		break;
-    	case "a.debug":
-    		CommandDebug.handle(this, event);
-    		break;
-    	default:
-    		event.getChannel().sendMessage("Command not found.");
-    		break;
-    	}
-    }
-    
-    @Override
-    public String toString() {
-    	return "[Shhhhhhh voiceChannelSnowflake="+this.voiceChannelSnowflake+",crew="+this.crew+",dead="+this.dead+"]";
-    }
+	}
+
+	public JDA getSession() {
+		return this.session;
+	}
+
+	@Override
+	public void onMessageReceived(MessageReceivedEvent event) {
+		Message msg = event.getMessage();
+		switch (msg.getContentRaw()) {
+		case "a.update":
+			CommandUpdate.handle(this, event);
+			break;
+		case "a.debug":
+			CommandDebug.handle(this, event);
+			break;
+		default:
+			event.getChannel().sendMessage("Command not found.");
+			break;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "[Shhhhhhh voiceChannelSnowflake=" + this.voiceChannelSnowflake + ",crew=" + this.crew + ",dead="
+				+ this.dead + "]";
+	}
 }
