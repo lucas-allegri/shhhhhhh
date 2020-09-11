5bc1e35293f49e419cedb472e9e151216e1b687e
Lucas Allegri <lucasallegri@protonmail.com>
2020-09-11 15:22:44 -0300

    Failsafe in case there's no mentions on a.dead, also mute on death

--- src/main/java/com/lucasallegri/CommandDead.java
+++ src/main/java/com/lucasallegri/CommandDead.java
@@ -1,5 +1,7 @@
 package com.lucasallegri;
 
+import java.util.List;
+
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
@@ -7,12 +9,24 @@ public class CommandDead {
 	
 	public static void handle(Shhhhhhh bot, MessageReceivedEvent evt) {
 		
+		List<Member> mentioned = evt.getMessage().getMentionedMembers();
+		
+		if(mentioned.size() < 0) {
+			evt.getChannel().sendMessage("No members mentioned.").queue();
+		}
+		
 		/* 	
 		* There's no real argument handling here, we'll simply take the mentions found on
 		* the message and add them members to the dead ArrayList.
 		*/
-		for(Member member : evt.getMessage().getMentionedMembers()) {
+		for(Member member : mentioned) {
 			bot.dead.add(member);
+			
+			/*
+			 *  We know they're dead now, so it's safe to issue a server mute right away
+			 *  instead of waiting till the next call.
+			 */
+			member.mute(true);
 		}
 	}
 
